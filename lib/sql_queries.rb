e7bc4a0a9acba421ff1adde71eb3680a7ec90396
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-16 19:48:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,9 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
-  FROM "
+  FROM users
+  JOIN pledges
+  "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
