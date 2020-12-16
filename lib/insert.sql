b1c856f48591f7296b61df212cd4d44d3e1be86b
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-16 19:21:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -25,7 +25,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Stain Proof Shirt', 'Clothing', '10000', '1-1-2020', '12-1-2020');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Automatic Bottle Opener', 'Kitchen', '5000', '10-1-2020', '12-1-2020');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Multi Function Skirt', 'Clothing', '11000', '5-1-2020', '11-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Cataan', 'Boardgame', '1000', '10-1-2010', '12-1-2020');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Whisk Cafe Recipe Book', 'Books', '1250', '2-1-2020', '4-1-2020');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Cataan', 'Boardgame', '1000', '10-1-2010', '12-1-2020');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Cataan', 'Boardgame', '1000', '10-1-2010', '12-1-2020');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Cataan', 'Boardgame', '1000', '10-1-2010', '12-1-2020');
