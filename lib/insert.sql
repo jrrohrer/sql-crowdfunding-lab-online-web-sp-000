99e2596742898305c458e4f39dbc42656fd50823
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-16 19:26:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,31 +1,5 @@
-INSERT INTO users (name, age) VALUES ('Sam', 19);
-INSERT INTO users (name, age) VALUES ('Allison', 20);
-INSERT INTO users (name, age) VALUES ('Dave', 21);
-INSERT INTO users (name, age) VALUES ('Greg', 22);
-INSERT INTO users (name, age) VALUES ('Bob', 23);
-INSERT INTO users (name, age) VALUES ('Lauren', 22);
-INSERT INTO users (name, age) VALUES ('Susie', 35);
-INSERT INTO users (name, age) VALUES ('Matt', 34);
-INSERT INTO users (name, age) VALUES ('Tim', 36);
-INSERT INTO users (name, age) VALUES ('Jessie', 35);
-INSERT INTO users (name, age) VALUES ('Evelyn', 19);
-INSERT INTO users (name, age) VALUES ('James', 17);
-INSERT INTO users (name, age) VALUES ('Gerry', 36);
-INSERT INTO users (name, age) VALUES ('Emily', 34);
-INSERT INTO users (name, age) VALUES ('Sarah', 30);
-INSERT INTO users (name, age) VALUES ('Katie', 32);
-INSERT INTO users (name, age) VALUES ('Carlin', 34);
-INSERT INTO users (name, age) VALUES ('Olivia', 18);
-INSERT INTO users (name, age) VALUES ('Owen', 16);
-INSERT INTO users (name, age) VALUES ('Ryan', 17);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Cataan', 'Boardgame', '1000', '10-1-2020', '12-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Hive', 'Boardgame', '5000', '11-1-2020', '1-1-2021');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('EEW Mini', 'Spinning Tools', '4000', '9-1-2020', '11-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Stain Proof Shirt', 'Clothing', '10000', '1-1-2020', '12-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Automatic Bottle Opener', 'Kitchen', '5000', '10-1-2020', '12-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Multi Function Skirt', 'Clothing', '11000', '5-1-2020', '11-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Whisk Cafe Recipe Book', 'Books', '1250', '2-1-2020', '4-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Snowfall Records', 'Music', '5000', '3-1-2020', '9-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Turtle Spindle', 'Spinning Tools', '1000', '7-1-2020', '10-1-2020');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Ways of Seeing', 'Art Exhibition', '5050', '10-1-2010', '12-1-2020');
